7393e93ee69140025c4d5ad90cd2189519c7f10d
XueJie1 <3124792285@qq.com>
2025-03-08 15:45:06 +0800

    添加功能：新增菜品、菜品分页查询

--- src/main/java/com/itheima/reggie/controller/DishController.java
+++ src/main/java/com/itheima/reggie/controller/DishController.java
@@ -1,15 +1,23 @@
 package com.itheima.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.itheima.reggie.common.R;
 import com.itheima.reggie.dto.DishDto;
+import com.itheima.reggie.entity.Category;
+import com.itheima.reggie.entity.Dish;
+import com.itheima.reggie.mapper.CategoryMapper;
+import com.itheima.reggie.service.CategoryService;
 import com.itheima.reggie.service.DishFlavorService;
 import com.itheima.reggie.service.DishService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.apache.commons.lang.StringUtils;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
@@ -19,10 +27,60 @@ public class DishController {
     private DishFlavorService dishFlavorService;
     @Autowired
     private DishService dishService;
+    @Autowired
+    private CategoryService categoryService;
+    @Autowired
+    private CategoryMapper categoryMapper;
 
+    /**
+     * 新增菜品
+     * @param dishDto
+     * @return 信息
+     */
     @PostMapping
     public R<String> save(@RequestBody DishDto dishDto) {
         log.info("菜品：{}", dishDto.toString());
-        return null;
+        dishService.saveWithFlavor(dishDto);
+        return R.success("新增菜品成功");
+    }
+
+    /**
+     * 菜品信息分页查询
+     * 由于 dish 表中只有分类id（categoryid），所以需要再从 category 表中查询分类名称并整合到返回值中。
+     * @param page 页码数，即第几页
+     * @param pageSize 一页有几项
+     * @param name 查询名字，可为空
+     * @return Page类型的分页数据
+     */
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize, String name) {
+        // 1. 分页构造器
+        Page<Dish> pageInfo = new Page<>(page, pageSize);
+        Page<DishDto> pageDto = new Page<>();
+        // 2. 条件构造器
+        LambdaQueryWrapper<Dish> queryWrapper = new LambdaQueryWrapper<>();
+        // 3. 添加过滤条件
+        queryWrapper.like(StringUtils.isNotEmpty(name) && name != null, Dish::getName, name);
+        // 4. 添加排序条件
+        queryWrapper.orderByDesc(Dish::getUpdateTime);
+        // 5. 进行分页查询
+        dishService.page(pageInfo, queryWrapper);
+
+        // 对象拷贝
+        BeanUtils.copyProperties(pageInfo, pageDto, "records");
+        List<Dish> records = pageInfo.getRecords();
+        List<DishDto> list =  records.stream().map((item) -> {
+            DishDto dishDto = new DishDto();
+            BeanUtils.copyProperties(item, dishDto);
+            Long categoryId = item.getCategoryId(); // 分类 ID
+            Category category = categoryService.getById(categoryId);
+            if (category != null) {
+                String categoryName = category.getName();
+                dishDto.setCategoryName(categoryName);
+            }
+            return dishDto;
+        }).collect(Collectors.toList());
+        pageDto.setRecords(list);
+        return R.success(pageDto);
     }
 }
--- src/main/java/com/itheima/reggie/service/impl/DishServiceImpl.java
+++ src/main/java/com/itheima/reggie/service/impl/DishServiceImpl.java
@@ -3,17 +3,42 @@ package com.itheima.reggie.service.impl;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.itheima.reggie.dto.DishDto;
 import com.itheima.reggie.entity.Dish;
+import com.itheima.reggie.entity.DishFlavor;
 import com.itheima.reggie.mapper.DishMapper;
+import com.itheima.reggie.service.DishFlavorService;
 import com.itheima.reggie.service.DishService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
+@Slf4j
 @Service
 public class DishServiceImpl extends ServiceImpl<DishMapper, Dish> implements DishService {
+    // 注入 口味 sevice ，存到 dish_flavor 口味表要用到
+    @Autowired
+    private DishFlavorService dishFlavorService;
     /**
      * 新增菜品并保存口味数据
      * @param dishDto
      */
+    @Transactional
     public void saveWithFlavor(DishDto dishDto) {
-        
+        // 保存菜品数据到数据库，由于 DishDto extends Dish，所以可以直接调 ServiceImpl<DishMapper, Dish> 的 save() 方法。
+        this.save(dishDto);
+
+        if (dishDto.getFlavors() != null) {
+            // 获取菜品 ID，之后要存到口味表里。
+            Long dishId = dishDto.getId();
+            List<DishFlavor> flavorList = dishDto.getFlavors(); // 此时 flavorList 中没有 dishId，所以要手动添加
+            for (int i = 0; i < flavorList.size(); i++) {
+                flavorList.get(i).setDishId(dishId);
+            }
+            dishFlavorService.saveBatch(flavorList);
+        } else {
+            log.info("没有口味，所以不会向 dish_flavor 表中插入数据。");
+        }
     }
 }
